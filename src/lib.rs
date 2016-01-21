2ea6ffdfbc92f1906c4dfc68336c386fd4f887e4
Gautier TANGUY <gautier.tanguy@gmail.com>
2016-01-21 22:41:15 +0100

    update version to 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-mod commands;
-mod errors;
-mod reader;
-mod redis;
-mod results;
-
 pub use errors::{ParsingError, RedisError};
 pub use redis::RedisClient;
 pub use results::RedisResult;
 pub use commands::{CommandBuilder, CommandSender, RedisCommand};
+
+pub mod commands;
+pub mod errors;
+pub mod reader;
+pub mod redis;
+pub mod results;
